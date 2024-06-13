724ea9c28e7ab77cba3f21a12e0017ca882a90c7
Michael Kozel <michael.kozel1@gmail.com>
2024-06-13 17:23:22 +0200

    Added repositories

---
+++ src/main/java/com/example/smartform/repositories/VillagePartRepository.java
@@ -0,0 +1,6 @@
+package com.example.smartform.repositories;
+
+import com.example.smartform.models.VillagePart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VillagePartRepository extends JpaRepository<VillagePart, String> {}
---
+++ src/main/java/com/example/smartform/repositories/VillageRepository.java
@@ -0,0 +1,7 @@
+package com.example.smartform.repositories;
+
+import com.example.smartform.models.Village;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VillageRepository extends JpaRepository<Village, String> {}
+
