098f2c71ad7e131b65741467806c3010f0bb7426
Michael Kozel <michael.kozel1@gmail.com>
2024-06-13 19:02:31 +0200

    Added deploy script

---
+++ deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+JAR_NAME="smartform.jar"
+
+IMAGE_NAME="smartform"
+
+echo "Building the project..."
+./gradlew clean build
+
+if [ $? -ne 0 ]; then
+    echo "Build failed. Exiting..."
+    exit 1
+fi
+
+echo "Building Docker image..."
+docker build -t $IMAGE_NAME .
+
+if [ $? -ne 0 ]; then
+    echo "Docker image build failed. Exiting..."
+    exit 1
+fi
+
+echo "Starting Docker Compose..."
+docker-compose up -d
+
+# Kontrola, zda Docker Compose byl spuštěn úspěšně
+if [ $? -ne 0 ]; then
+    echo "Docker Compose failed to start. Exiting..."
+    exit 1
+fi
+
+echo "Deployment completed successfully!"
